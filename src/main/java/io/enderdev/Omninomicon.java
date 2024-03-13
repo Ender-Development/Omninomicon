7f670f98609c54cd7cfa0f692c4baab191df8801
MasterEnderman <xxmr.endermanxx@gmail.com>
2024-03-13 01:10:36 +0100

    rebrand

---
+++ src/main/java/io/enderdev/Omninomicon.java
@@ -0,0 +1,11 @@
+package io.enderdev;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Omninomicon.MODID, name = Omninomicon.MODNAME, acceptedMinecraftVersions = Omninomicon.VERSIONS)
+public class Omninomicon {
+    public static final String MODID = "omninomicon";
+    public static final String MODNAME = "Omninomicon";
+    public static final String CFGDIR = "Omninomicon/";
+    public static final String VERSIONS = "[1.12.2]";
+}
